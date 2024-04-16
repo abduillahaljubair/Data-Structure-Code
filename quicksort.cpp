cebb92501d2a58d2e4c40b081447d606da962154
abdullahaljubair <abdullahaljubair2019@gmail.com>
2024-04-16 11:07:52 +0600

    hi

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,63 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int>a(10);
-int partition(int low,int high)
-{
-int i=low;
-int j=high;
-int pivot=a[low];
-while(i<j)
-{ 
-    while(a[i]<=pivot)
-     {
-         i++;
-     }  
-    while(a[i]>pivot)
-     {
-      j--;
-     }
 
+vector<int> a;
 
-    if(i<j)
-     {
-     swap(a[i],a[j]);
-     }
+int partition(int low, int high) {
+    int pivot = a[low];
+    int i = low + 1;
+    int j = high;
+
+    while (i <= j) {
+        while (i <= j && a[i] <= pivot) {
+            i++;
+        }
+        while (i <= j && a[j] > pivot) {
+            j--;
+        }
+        if (i < j) {
+            swap(a[i], a[j]);
+        }
+    }
+    swap(a[low], a[j]);
+    return j;
 }
- swap(a[low],a[j]);
-return j;
-}
-void quicksort(int low,int high)
-{
-    if(low<high)
-    {
-        int j=partition(low,high);
-        partition(low,j-1);
-        partition(j+1,high);
 
+void quicksort(int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(low, high);
+        quicksort(low, pivotIndex - 1);
+        quicksort(pivotIndex + 1, high);
     }
 }
-void printing()
-{
-     for(int i=0;i<5;i++)
-    {
-        cout<<a[i]<<" ";
-    }
 
+void printing(int n) {
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
 }
-int main()
-{
-    vector<int>c;
-    int ele,n;
-    cout<<"enter total element";
-    cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        cin>>ele;
-        c.push_back(ele);
+
+int main() {
+    int n;
+    cout << "Enter total number of elements: ";
+    cin >> n;
+    
+    cout << "Enter the elements: ";
+    a.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
-    quicksort(0,n-1);
 
-    printing();
+    quicksort(0, n - 1);
+    
+    cout << "Sorted array: ";
+    printing(n);
 
-
+    return 0;
 }
